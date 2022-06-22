1e0c601822d0257c18ec36bc13bedce4a9efc392
BenSwartz <odanata410@gmail.com>
2022-06-22 11:11:25 -0500

    Repo Start

---
+++ app.js
@@ -0,0 +1,20 @@
+/*
+board looks like
+[0] [1] [2]
+[3] [4] [5]
+[6] [7] [8]
+
+winning lines
+[0] [1] [2]
+[3] [4] [5]
+[6] [7] [8]
+[0] [3] [6]
+[1] [4] [7]
+[2] [5] [8]
+[0] [4] [8]
+[2] [4] [6]
+*/
+
+const name1 = document.getElementById('player1').value
+const name2 = document.getElementById('player2').value
+
