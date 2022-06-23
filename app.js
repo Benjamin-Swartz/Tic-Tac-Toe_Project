f77da07cd8bac0249ce6a3997462e071e11f8003
BenSwartz <odanata410@gmail.com>
2022-06-23 08:31:49 -0500

    minor twiks

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ winning lines
 [2] [4] [6]
 */
 
-const name1 = document.getElementById('player1').value
-const name2 = document.getElementById('player2').value
+const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
