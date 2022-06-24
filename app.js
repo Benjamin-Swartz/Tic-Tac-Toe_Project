bcc328b738d1ab3d7d0dce79d4e204cd380f1b6a
BenSwartz <odanata410@gmail.com>
2022-06-23 19:59:05 -0500

    framework for functions

--- app.js
+++ app.js
@@ -25,20 +25,38 @@ const winningLines = [
     [0, 4, 8],
     [2, 4, 6],
 ]
-
+let playerOturn = flase
 const Xplayer = 'x'
 const Oplayer = 'o'
 const restartButton = document.getElementById('resetButton')
 const playingSpaces = document.querySelectorAll('space')
 const board = document.getElementById('board')
+const winnerMessage = document.getElementById('winner-message')
 
 startGame()
 
 restartButton.addEventListener('click',  startGame())
 
 function startGame(){
+    playerXturn = flase
     playingSpaces.forEach(space => {
         space.classList.remove(Xplayer)
         space.classList.remove(Oplayer)
+
     })
+    winnerMessage.classList.remove('show')
 }
+
+function markPlace(spc){
+    const space = spc.target
+    const currentPlayer = playerOturn ? Xplayer : Oplayer
+    placeMark(cell, currentPlayer)
+    if(checkWin(currentPlayer)){
+        endGame(false)
+    } else if (isDraw()){
+        endGame(true)
+    } else {
+        switchTurns()
+    }
+}
+
